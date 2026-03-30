3fd0337ab237e61fdc728fa8da4f2a9d62350b40
Lukas Bals <mail@lukasbals.at>
2026-03-30 21:51:37 +0200

    docs: make living planet report the main reference

--- ui/components/dashboard/data.ts
+++ ui/components/dashboard/data.ts
@@ -1,11 +1,15 @@
 export const references = [
   {
-    label: "IUCN Red List Categories and Criteria",
-    href: "https://www.iucnredlist.org/resources/categories-and-criteria",
+    label: "Living Planet Report",
+    href: "https://livingplanet.panda.org/",
   },
   {
     label: "Living Planet Database",
     href: "https://www.livingplanetindex.org/data_portal",
+  },
+  {
+    label: "IUCN Red List Categories and Criteria",
+    href: "https://www.iucnredlist.org/resources/categories-and-criteria",
   },
   {
     label: "GBIF Biodiversity Data",
